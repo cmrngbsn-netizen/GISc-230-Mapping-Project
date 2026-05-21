0e6b75343c89ca889ac85ef3f460d6ec9fd7a503
cmrngbsn-netizen <cmrn.gbsn@gmail.com>
2026-05-21 16:44:00 -0700

    Added City Size Column

--- Chapter02/js/main_with_debug.js
+++ Chapter02/js/main_with_debug.js
@@ -2,63 +2,93 @@
 
 // Debugging Notes:
 // Fixed issues with table generation and event handling.
-// Corrected loop references and verified DOM loading behavior.
-// Confirmed hover and click interactions function properly.
-// Used browser console and testing to identify and resolve errors.
+// Added city size classification logic.
+// Verified hover and click interactions.
+// Used console testing to remove errors.
 
-// This function runs when the page has finished loading.
+// Initialize script after DOM loads
 function initialize() {
     cities();
 }
 
-// This function creates a table showing city and population data.
+// Function to create the city table
 function cities() {
+
+    // Array of city objects
     var cityPop = [
-        { city: 'Madison', population: 233209 },
-        { city: 'Milwaukee', population: 594833 },
-        { city: 'Green Bay', population: 104057 },
-        { city: 'Superior', population: 27244 }
+        {
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
     ];
 
+    // Create table element
     var table = document.createElement("table");
+
+    // Create header row
     var headerRow = document.createElement("tr");
 
-    headerRow.insertAdjacentHTML("beforeend", "<th>City</th><th>Population</th>");
+    // Add headers
+    headerRow.insertAdjacentHTML(
+        "beforeend",
+        "<th>City</th><th>Population</th><th>City Size</th>"
+    );
+
     table.appendChild(headerRow);
 
+    // Loop through city data
     for (var i = 0; i < cityPop.length; i++) {
-        var cityName;
 
-        if (cityPop[i].city == 'Madison') {
-            cityName = 'Badgerville';
-        } else if (cityPop[i].city == 'Green Bay') {
-            cityName = 'Packerville';
+        // Determine city size using conditionals
+        var citySize;
+
+        if (cityPop[i].population < 100000) {
+            citySize = 'Small';
+        } else if (cityPop[i].population < 500000) {
+            citySize = 'Medium';
         } else {
-            cityName = cityPop[i].city;
+            citySize = 'Large';
         }
 
-        var popValue;
+        // Create row HTML
+        var rowHtml =
+            "<tr>" +
+            "<td>" + cityPop[i].city + "</td>" +
+            "<td>" + cityPop[i].population + "</td>" +
+            "<td>" + citySize + "</td>" +
+            "</tr>";
 
-        if (cityPop[i].population < 500000) {
-            popValue = cityPop[i].population;
-        } else {
-            popValue = 'Too big!';
-        }
-
-        var rowHtml = "<tr><td>" + cityName + "</td><td>" + popValue + "</td></tr>";
-        table.insertAdjacentHTML("beforeend", rowHtml);
+        // Add row to table
+        table.insertAdjacentHTML('beforeend', rowHtml);
     }
 
+    // Add table to webpage
     document.querySelector("#mydiv").appendChild(table);
 
+    // Add interactions
     addEvents();
 }
 
-// This function adds hover and click interactions to each city row.
+// Add hover and click events
 function addEvents() {
+
     var rows = document.querySelectorAll("tr");
 
     for (var i = 1; i < rows.length; i++) {
+
+        // Hover effect
         rows[i].addEventListener("mouseover", function() {
             this.style.color = "red";
         });
@@ -67,14 +97,16 @@ function addEvents() {
             this.style.color = "black";
         });
 
+        // Click interaction
         rows[i].addEventListener("click", function() {
 
-    var cityName = this.cells[0].innerHTML;
+            var cityName = this.cells[0].innerHTML;
 
-    alert("You clicked on " + cityName);
+            alert("You clicked on " + cityName);
 
-});
+        });
     }
 }
 
+// Run initialize after DOM loads
 document.addEventListener("DOMContentLoaded", initialize);
